f544ccbbf055c0980c35e9688c9136d244ac55d6
franklin <igahfranklyn@gmail.com>
2026-06-09 08:43:26 +0100

    auth js

--- auth.ts
+++ auth.ts
@@ -56,5 +56,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
     signIn: "/login",
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET,
+  trustHost: true,
 });
