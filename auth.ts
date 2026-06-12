2bb70c2a68adbca100618c82dd484f37872f83cb
franklin <igahfranklyn@gmail.com>
2026-06-12 03:27:52 +0100

    auth token

--- auth.ts
+++ auth.ts
@@ -56,6 +56,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
     signIn: "/login",
   },
+  // secret: process.env.NEXTAUTH_SECRET,
   secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET,
   trustHost: true,
 });
